d85e27e06eeebe432bdc18cb305868f0eed92a3c
Vladimir <vtattyba@buffalo.edu>
2019-10-17 00:18:55 -0400

    Application and Routes for Login and Create Account

--- katz/katz/urls.py
+++ katz/katz/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', include('login.urls')),
+    path('createaccount/', include('createAccount.urls')),
 ]
 
 from django.urls import include
