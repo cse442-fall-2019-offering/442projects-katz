f18e707a210fd01308ac1f71dccca55ee03453b0
gdzanell <gdzanell@buffalo.edu>
2019-10-23 21:10:04 -0400

    create account added name fields #32

--- 442projects-katz-develop/katz/users/views.py
+++ 442projects-katz-develop/katz/users/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib import  messages
-from django.shortcuts import redirect
+from django.contrib import messages
+from users.utils.forms import SignUpForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
+            messages.success(request, f'Account created for {username}!')
+            login(request, user)
+            return redirect('home.html')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'users/createAccount.html', {'form': form})
