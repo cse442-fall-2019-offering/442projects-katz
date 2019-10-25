81d12568598660ff1b5e3d8217cd5aa9835b2ec0
gdzanell <gdzanell@buffalo.edu>
2019-10-25 13:23:59 -0400

    added tests

---
+++ katz/teamapp/tests/test_views.py
@@ -0,0 +1,28 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from teamapp.models import School, Class, EmailURL, Team, Student, EnrolledIn, Teams
+import json
+
+class TestViews(TestCase):
+
+    #Holds the create client that is used on all other test cases
+    def setUp(self):
+        self.client = Client()
+        self.homepage_url = reverse('homepage')
+        self.classpage_url = reverse('homepage')
+
+    #tests if the homepage function acceses the template
+    def test_homepage_GET(self):
+        print("Tests if homepage function acceses the template")
+        response = self.client.get(self.homepage_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'homepage.html')
+
+    #tests if the classpage function acceses the template
+    def test_classpage_GET(self):
+        print("Tests if classpage function acceses the template")
+        response = self.client.get(self.classpage_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'homepage.html')
